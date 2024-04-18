990aa7f84147f5fcadb6a56e7b01ddd814a1c718
Arzav Jain <arzav.jain@gmail.com>
2024-04-18 15:55:57 +0530

    JSX structure and functional components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const isMorning = false
+const morningText = "Good Morning user"
+const afternoobText = "Good Afternoon user"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const handleOnClick = () => {
+    console.log("i was clicked");
+}
+
+
+//everything that is wrapped inside {} will be treated as javascript.
+const morningElement = <div> {morningText} <span>It is 8AM</span> </div>
+const afternoonElement = <div> {afternoobText} <span onClick={() => handleOnClick(isMorning)}>It is 6PM</span> </div>
+const greetingElement = isMorning ? morningElement : afternoonElement 
+
+
+// Functional components are regular functions that return react elements
+const greetingComponent = () => {
+    return(
+        <>
+        <div>
+            {greetingElement}
+            i am inside a component now.
+        </div>
+        </>
+    )
+}
+
+console.log(greetingElement)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(greetingComponent())
