668ac3e815616c6fde646694b4f18807632745d5
Arzav Jain <arzav.jain@gmail.com>
2024-04-25 13:58:17 +0530

    lazy api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, {useState, useCallback, useEffect, useRef, useMemo} from 'react';
+import React, {useState, useCallback, useEffect, useRef, useMemo, lazy, Suspense} from 'react';
 import Button from './Button'
-import Text from './Text'
+// import Text from './Text'
 import Timer from './Timer'
 import ButtonWithTooltip from './ButtonWithTooltip'
 import Input from './Input'
 import SecondParent from './SecondParent'
 import PrintTable from './PrintTable'
+
+//lazy lets you defer loading component's code until it is rendered for the first time.
+//Suspense allows you to suspend rendering while data is being fetched and renders when data is fetched.
+const Text = lazy(()=> delayForDemo(import('./Text.js')));
+
+async function delayForDemo(promise) {
+  await new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  });
+  return promise;
+}
+
 const App = () => {
-
-    //Whenever we pass non-primitive data types to the component, it causes the component to re-render and thus
-    //impacting the performace or slowing it down.
-    //Non-primitive data types are: objects, arrays, functions
-    //Primitive data types are: strings, numbers, booleans, undefined, null
-    //useMemo does not allow re-render if the dependency array does not change, and thus optimises the performance
-
-    const [counter1, setCounter1] = useState(0)
-    const [counter2, setCounter2] = useState(0)
-    // const [myObj, setMyObj] = useState({channel: "JS Cafe"})
-    const myObj = useMemo(() => ({ channel: "JS Cafe" }), []);
-    const arr = useMemo(() => (['a', 'b']), [])
-    const val = 10
+  const[showText, toggleText] = useState(false);
 
     return <>
-        Counter1: {counter1} <button onClick={() => setCounter1((prev) => prev+1)}>count1++</button>
-        <br/>
-        Counter2: {counter2} <button onClick={() => setCounter2((prev) => prev+1)}>count2++</button>
-        <br/>
-        {/* <PrintTable num={counter1} obj={myObj} val={val}/> */}
-        <PrintTable num={counter1} arr={arr} obj={myObj} val={val}/>
+    <button onClick={()=> {toggleText((prev) => !prev)}} >Toggle Text</button>
+    {showText && <Suspense fallback = {<div>I am loading</div>}>
+    <Text>Hello Guys!</Text>
+    </Suspense>}
+    
     </>
 }
 
