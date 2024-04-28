3ee17ad68bb4a935f4b236a7e1ce820f9bedc5f6
Arzav Jain <arzav.jain@gmail.com>
2024-04-28 17:11:50 +0530

    react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./styles.css";
-import { Provider } from 'react-redux'
-import {store} from './store'
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<Provider store={store}>
-<App/>
-</Provider>
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
