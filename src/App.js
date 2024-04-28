e24328344b66a25db5ece904720840779d6991d1
Arzav Jain <arzav.jain@gmail.com>
2024-04-28 15:55:09 +0530

    Redux and Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, {useState, useCallback, useEffect, useRef, useMemo, lazy, Suspense} from 'react';
+import React, {useState} from 'react';
+import { createPortal } from 'react-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, incByAmount } from './counterSlice'
 import Button from './Button'
 // import Text from './Text'
 import Timer from './Timer'
@@ -7,18 +10,17 @@ import Input from './Input'
 import SecondParent from './SecondParent'
 import PrintTable from './PrintTable'
 import Heading from './Heading';
-import { themeContext } from './context';
 import Navbar from './Navbar';
-import { createPortal } from 'react-dom';
+import {ThemeContext} from './context'
 
 const App = () => {
-
-//createPortal lets you render some children into a different part of the DOM.
-  const [showModal, toggleModal] = useState(false);
-
+  const count = useSelector((state) => state.counter.counterValue)
+  const dispatch = useDispatch()
     return <>
-    Hey I am inside root <button onclick = {() => toggleModal((prev) => !prev)}>Toggle Modal</button>
-    {showModal && createPortal(<div>This is a modal content</div>, document.body)}
+    {count}
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incByAmount(5))}>Inc by amount</button>
     </>
 }
 
